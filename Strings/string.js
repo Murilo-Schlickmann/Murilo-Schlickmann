3691a76088f40a5eb5c1f795d463665159e317b5
Murilo Voigt Schlickmann <murilo.praesoft@gmail.com>
2022-05-11 09:32:51 -0300

    template-strings

---
+++ Strings/string.js
@@ -0,0 +1,6 @@
+const nome = prompt("Digite seu nome")
+const cor = prompt("Digite sua cor favorita")
+console.log("a cor favorita de " + nome + " é " + cor)
+
+console.log(`A cor favorita de ${nome} é ${cor}`)
+
